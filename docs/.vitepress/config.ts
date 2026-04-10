67834a1395cbad440c7a268b26260199d806a16b
Pablo Alcaraz Martínez <pablo@alcarazm.es>
2026-04-10 23:09:01 +0200

    docs: add meta tag for SEO

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,12 +2,20 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
   title: " ",
-  description: "Component Lifecycle documentation",
+  description: "A lightweight, framework-agnostic lifecycle engine for DOM components with typed events, a strict state machine, and ergonomic event APIs.",
   base: '/component-lifecycle/',
 
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['link', { rel: 'icon', type: 'image/png', href: '/favicon.png' }]
+    ['link', { rel: 'icon', type: 'image/x-icon', href: '/component-lifecycle/favicon.ico' }],
+    ['link', { rel: 'icon', type: 'image/png', href: '/component-lifecycle/favicon.png' }],
+    ['meta', { name: 'author', content: 'https://github.com/palcarazm' }],
+    ['meta', { name: 'robots', content: 'index, follow' }],
+    ['meta', { name: 'revisit-after', content: '1 month' }],
+    ['meta', { property: 'og:description', content: 'A lightweight, framework-agnostic lifecycle engine for DOM components with typed events, a strict state machine, and ergonomic event APIs.' }],
+    ['meta', { property: 'og:url', content: 'https://github.com/palcarazm/component-lifecycle' }],
+    ['meta', { property: 'og:image', content: 'https://github.com/palcarazm/component-lifecycle/card.png' }],
+    ['meta', { property: 'og:image:width', content: '728' }],
+    ['meta', { property: 'og:image:height', content: '364' }],
   ],
 
   themeConfig: {
